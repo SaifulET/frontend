783e55d2952dbf7e04e4a94043cc8b46f71f2153
Saiful Islam <132368544+SaifulET@users.noreply.github.com>
2025-06-18 12:40:22 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <div className='w-full h-10 text-xl   text-white flex items-center justify-center flex-col pr-10' >
             <div className={`  flex items-center justify-end  text-yellow-200 font-mono ${text==="Login"?"hidden":"block"}`}><img src="https://static.vecteezy.com/system/resources/previews/024/983/914/non_2x/simple-user-default-icon-free-png.png" alt="img" width={80} height={80} className='rounded-full bg-white text-center' /></div>
             <div className=''>{text}</div>
-            <div className={`text-gray-300 font-thin hover:underline `}>
+            <div className={`text-gray-300 font-thin hover:underline ${text==="Login"?"hidden":"block"} `}>
             <Link to="/profile">View Profile</Link>
             </div>
             <div >
